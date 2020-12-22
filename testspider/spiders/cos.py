ca032b5a4f8a9bf2dcc23fe382a6e8b74b94baea
zhangxy <zhang@qq.com>
2020-12-22 20:53:20 +0800

    页面解析

--- testspider/spiders/cos.py
+++ testspider/spiders/cos.py
@@ -8,8 +8,20 @@ class CosSpider(scrapy.Spider):
     start_urls = ['http://dl.ixxcc.com/NSFW/Cosplay/']
 
     def parse(self, response):
-        filenames = response.css('.file')
-        for name in filenames:
-            item = TestspiderItem()
-            item['filename'] = name.css('.name::text').extract_first()
-            yield item
+        filenames = response.xpath('//tr[@class="file"]//span/text()').extract()
+        picsurls = response.xpath('//tr[@class="file"]//a/@href').extract()
+        # urls = response.urljoin(picsurl)
+
+        for index in range(len(filenames)):
+            trueurl = response.urljoin(picsurls[index])
+
+            print(filenames[index])
+            print(trueurl)
+            yield scrapy.Request(url=trueurl, meta={'filename': filenames[index]}, callback=self.parse2)
+
+    def parse2(self, response):
+        filename = response.meta['filename']
+        picnames = response.xpath('//tr[@class="file"]//a//span/text()').extract()
+        urls = response.xpath('//tr[@class="file"]//a/@href').extract()
+        for url in urls:
+            downloadurl = response.urljoin(url)
